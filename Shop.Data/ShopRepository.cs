8ee8acb5a6a8c39b15a6c9e91567319ca789bde8
Oleg <olegshitikov@gmail.com>
2020-08-08 01:22:54 +0300

    Create project structure

---
+++ Shop.Data/ShopRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Shop.Data
+{
+    public class ShopRepository : IShopRepository
+    {
+        public int x()
+        {
+            return 1;
+        }
+    }
+}
